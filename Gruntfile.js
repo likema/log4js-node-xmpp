e1b4b44977b924db04241de08a9cf15afd9c03b0
Like Ma <likemartinma@gmail.com>
2014-03-09 02:06:29 +0800

    Support jslint checking by grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,20 @@ module.exports = function (grunt) {
                 jshintrc: true
             },
             files: ['*.js', 'examples/*.js']
+        },
+
+        jslint: {
+            server: {
+                src: ['*.js', 'examples/*.js'],
+                directives: {
+                    node: true
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.registerTask('default', ['jshint:files']);
+    grunt.registerTask('default', ['jshint:files', 'jslint']);
 };
 // vim: ts=4 sw=4 sts=4 et:
